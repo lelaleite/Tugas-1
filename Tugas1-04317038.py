b9d2aeb21c5f377f5f9741bf33909c370a147f1f
lelaleite <32257565+lelaleite@users.noreply.github.com>
2017-09-25 13:59:06 +0700

    Add files via upload

---
+++ Tugas1-04317038.py
@@ -0,0 +1,10 @@
+nama = "Elisabet Maria Pereira da Costa Leite"
+nim = "04317038"
+umur = 18
+print ("Namaku :" + nama)
+print ("Nimku :" + nim)
+print ("umur :")
+print (umur)
+print ("umurku 5 tahun lagi :")
+print (umur + 5)
+
